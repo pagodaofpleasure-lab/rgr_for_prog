00eae66855d6e0a0d475de254ee667a87f6ebc09
D1cented <luhnevsemen38@gmail.com>
2026-06-19 22:06:59 +0700

    Added auto keygen for aes

--- main_aes.cpp
+++ main_aes.cpp
@@ -1,6 +1,8 @@
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <ctime>
 #include <string>
 #include "aes.h"
 
@@ -12,12 +14,31 @@ void print_help() {
     std::cout << "Output will be saved to 'output'.\n\n";
 }
 
+
+void auto_key(std::string& str){
+	if (str=="a"){
+		str.clear();
+		str.reserve(128);
+		for (int i = 0; i < 16; ++i) str.push_back(static_cast<char>(std::rand()%256));
+	}
+}
+
+
+
+
+
 int main() {
     print_help();
+    std::srand(std::time(NULL));
+
 
     std::string key_str, mode;
-    std::cout << "Key (16 chars): ";
+    std::cout << "Key (16 chars) [a for auto]: ";
     std::cin >> key_str;
+    auto_key(key_str);
+    
+    std::cout<<"\n"<<key_str<<"\n";
+
     if (key_str.length() != 16) {
         std::cerr << "Key must be 16 characters!\n";
         return 1;
@@ -26,7 +47,7 @@ int main() {
     std::cout << "Mode (e/d): ";
     std::cin >> mode;
 
-    std::ifstream in("input", std::ios::binary);
+    std::ifstream in("input4aes", std::ios::binary);
     std::ofstream out("output", std::ios::binary);
     if (!in || !out) {
         std::cerr << "File error!\n";
@@ -36,7 +57,7 @@ int main() {
     std::vector<uint8_t> key(key_str.begin(), key_str.end());
     AES aes(key);
 
-    const int64_t buffer_size = 1024 * 64; // 64KB buffer
+    const int64_t buffer_size = 1024 * 64; // 64KB 
     std::vector<uint8_t> buffer(buffer_size);
     uint8_t block_in[16], block_out[16];
 
@@ -48,7 +69,7 @@ int main() {
                 aes.encrypt(buffer.data() + i, block_out);
                 out.write(reinterpret_cast<char*>(block_out), 16);
             }
-            if (in.eof()) { // Padding PKCS#7
+            if (in.eof()) { // Padding 
                 uint8_t padding_val = 16 - (bytes_read - i);
                 std::vector<uint8_t> last_block(16, padding_val);
                 for (int64_t j = 0; j < (bytes_read - i); ++j) last_block[j] = buffer[i + j];
@@ -67,7 +88,7 @@ int main() {
             for (int64_t i = 0; i < bytes_read; i += 16) {
                 aes.decrypt(buffer.data() + i, block_out);
                 processed += 16;
-                if (processed == total_size) { // Remove padding
+                if (processed == total_size) { // Rm padding
                     uint8_t pad_len = block_out[15];
                     if (pad_len <= 16) out.write(reinterpret_cast<char*>(block_out), 16 - pad_len);
                 } else {
